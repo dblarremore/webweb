659b0651199dbcf627c5e8acb0243063353439d7
Hunter Wapman <hunter.wapman@gmail.com>
2019-01-14 14:11:11 -0700

    simplified nx layers example

--- examples/python (networkx)/layers.py
+++ examples/python (networkx)/layers.py
@@ -1,52 +1,33 @@
 from webweb import Web
 import networkx as nx
 
-snake1 = nx.Graph()
-snake1.add_edges_from([[0, 1], [1, 2], [2, 3]])
+snake = nx.Graph()
+snake.add_edges_from([[0, 1], [1, 2], [2, 3]])
 
-snake1.nodes[0]['isHead'] = True
-snake1.nodes[1]['isHead'] = False
-snake1.nodes[2]['isHead'] = False
-snake1.nodes[3]['isHead'] = False
+snake.nodes[0]['isHead'] = True
+snake.nodes[1]['isHead'] = False
+snake.nodes[2]['isHead'] = False
+snake.nodes[3]['isHead'] = False
 
-web = Web(
-    title='oroboros',
-    nx_G=snake1,
-)
+web = Web(title='oroboros', nx_G=snake)
 
-snake2 = nx.Graph()
-snake2.add_edges_from([[0, 1], [1, 2], [2, 3], [3, 0]])
+snake.add_edge(0, 3)
 
-snake2.nodes[0]['isHead'] = True
-snake2.nodes[1]['isHead'] = False
-snake2.nodes[2]['isHead'] = False
-snake2.nodes[3]['isHead'] = False
+web.networks.oroboros.add_layer(nx_G=snake)
 
-web.networks.oroboros.add_layer(nx_G=snake2)
+snake.remove_node(3)
+snake.add_edge(0, 2)
 
-snake3 = nx.Graph()
-snake3.add_edges_from([[0, 1], [1, 2], [2, 0]])
+web.networks.oroboros.add_layer(nx_G=snake)
 
-snake3.nodes[0]['isHead'] = True
-snake3.nodes[1]['isHead'] = False
-snake3.nodes[2]['isHead'] = False
+snake.remove_node(2)
+snake.add_edge(0, 1)
 
-web.networks.oroboros.add_layer(nx_G=snake3)
+web.networks.oroboros.add_layer(nx_G=snake)
 
-snake4 = nx.Graph()
-snake4.add_edges_from([[0, 1], [1, 0]])
+snake.remove_node(1)
 
-snake4.nodes[0]['isHead'] = True
-snake4.nodes[1]['isHead'] = False
-
-web.networks.oroboros.add_layer(nx_G=snake4)
-
-snake5 = nx.Graph()
-snake5.add_node(0)
-
-snake5.nodes[0]['isHead'] = True
-
-web.networks.oroboros.add_layer(nx_G=snake5)
+web.networks.oroboros.add_layer(nx_G=snake)
 
 # display the first layer first (you could put, say, 1 here and it would display the second)
 web.display.networkLayer = 0
