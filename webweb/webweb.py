0c9cb8b41fd93699a227852a6efeb0b0912e17c6
Hunter Wapman <hunter.wapman@gmail.com>
2019-01-20 14:03:31 -0700

    pydoc testing

--- webweb/webweb.py
+++ webweb/webweb.py
@@ -137,18 +137,18 @@ class Network(dict):
     def add_layer(self, adjacency=None, adjacency_type=None, nodes=None,  metadata=None, nx_G=None):
         """adds a layer to the network.
 
-        :param adjacency: edge list or adjacency matrix
-        :param adjacency_type: string. 'matrix' or 'edge list'. Supply if passing an adjacency matrix with fewer than 3 nodes
-        :param nodes: dict of node attribute dicts
-        :param metadata: dict of vectorized metadata and display information. 
-        {
+        - adjacency: edge list or adjacency matrix
+        - adjacency_type: string. 'matrix' or 'edge list'. Supply if passing an adjacency matrix with fewer than 3 nodes
+        - nodes: dict of node attribute dicts
+        - metadata: dict of vectorized metadata and display information. 
+        ```{
             'attribute' : {
                 'values' : [ "attribute_value", ...],
                 'categories' : ["category1", "category2", ...] (supply if values is categorical but contains numbers; values in the `values` array will be used as indexes to this array)
                 'type' : string. Only necessary if displaying binary information with 0/1 and not True/False.
             }
-        }
-        :param nx_G: a networkx graph.
+        }```
+        - nx_G: a networkx graph.
 
         call with at least one of:
         - adjacency
