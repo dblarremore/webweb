ccd0a02aa768be3dec4474e410e6fadc2aadbbac
Hunter Wapman <hunter.wapman@gmail.com>
2019-01-21 09:32:40 -0700

    trying to get python working

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="webweb",
-    version="0.0.1",
-    author="Dan Larremore",
-    author_email="",
+    version="0.0.2",
+    author="Hunter Wapman",
+    author_email="hunter.wapman@gmail.com",
     description="webweb: an easy-to-use network visualization tool",
     long_description="webweb: an easy-to-use network visualization tool",
     long_description_content_type="text/markdown",
-    url="https://github.com/dblarremore/webweb",
+    url="https://webwebpage.github.io",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
