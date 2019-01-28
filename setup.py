b2a82e3512327bcf517d34aaacfe12912d1cb597
Hunter Wapman <hunter.wapman@gmail.com>
2019-01-28 16:03:49 -0700

    also fixing version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read_requirements(filename):
 
 setup(
     name="webweb",
-    version="0.0.7",
+    version="0.0.8",
     author="Dan Larremore; Hunter Wapman",
     author_email="hunter.wapman@gmail.com",
     description="webweb: an easy-to-use network visualization tool",
